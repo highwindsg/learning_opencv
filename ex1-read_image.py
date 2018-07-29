9bc2622aed57ec92784a0c060f6bb99788dede31
highwind.sg <highwind.sg@gmail.com>
2018-07-29 21:57:46 +0800

    Initial commit

---
+++ ex1-read_image.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+#read the picture in grayscale first
+img = cv2.imread('house1.jpg', cv2.IMREAD_GRAYSCALE)
+
+#this will display the image in grayscale
+cv2.imshow('image',img)
+
+#click on the gray picture and waiting for user to press any key
+cv2.waitKey(0)
+
+#once any key is pressed, cv2 will save to the new filename
+cv2.imwrite('house1gray.png', img)
+
+#and after saving the new filename, will then close the gray picture file
+cv2.destroyAllWindows()
+
